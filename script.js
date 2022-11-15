3afc3bb38ea795981a05d711c0931c51686fa9cb
KirMac <komarova1610@yandex.ru>
2022-11-15 23:15:44 +0300

    first version of combinator project

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function () {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        const uploaded_image = reader.result;
+        document.querySelector(".starter").classList.add("hidden");
+        document.querySelector(".logo").classList.remove("hidden");
+        document.querySelector("#display-image").classList.remove("hidden");
+        document.querySelector(
+            "#display-image"
+        ).style.backgroundImage = `url(${uploaded_image})`;
+    });
+    reader.readAsDataURL(this.files[0]);
+});
