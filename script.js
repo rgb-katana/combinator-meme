18a398741629308cdf4357ef649a000f6d7d7371
rgb-katana <komarova1610@yandex.ru>
2022-11-16 16:38:50 +0300

    added moving up and down functionality

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
-"use strict";
+'use strict';
 
-const image_input = document.querySelector("#image-input");
-image_input.addEventListener("change", function () {
+function startCombinating() {
+  // Select image input
+  const image_input = document.querySelector('#image-input');
+  // Listen to input and entire logic.
+  image_input.addEventListener('change', function () {
     const reader = new FileReader();
-    reader.addEventListener("load", () => {
-        const uploaded_image = reader.result;
-        document.querySelector(".starter").classList.add("hidden");
-        document.querySelector(".logo").classList.remove("hidden");
-        document.querySelector("#display-image").classList.remove("hidden");
-        document.querySelector(
-            "#display-image"
-        ).style.backgroundImage = `url(${uploaded_image})`;
+    reader.addEventListener('load', () => {
+      const uploaded_image = reader.result;
+      document.querySelector('.starter').classList.add('hidden');
+      document.querySelector('.caption').classList.remove('hidden');
+      document.querySelector('#display-image').classList.remove('hidden');
+      document.querySelector(
+        '#display-image'
+      ).style.backgroundImage = `url(${uploaded_image})`;
     });
     reader.readAsDataURL(this.files[0]);
-});
+  });
+
+  // Selecting phase.
+  const handler = document.querySelector('.caption--handler');
+  const entireCaption = document.querySelector('.caption');
+  let top = 80;
+  let left = 50;
+  handler.addEventListener('click', function (e) {
+    const elem = e.target;
+    console.log(elem);
+    if (elem.classList.contains('move--up-1')) {
+      if (entireCaption.style.top === '10%') return;
+      top--;
+      entireCaption.style.top = `${top}%`;
+    }
+
+    if (elem.classList.contains('move--down-1')) {
+      if (entireCaption.style.top === '85%') return;
+      top++;
+      entireCaption.style.top = `${top}%`;
+    }
+
+    if (elem.classList.contains('move--up-10')) {
+      console.log(parseInt(entireCaption.style.top));
+      if (parseInt(entireCaption.style.top) < 20) return;
+      top -= 10;
+      entireCaption.style.top = `${top}%`;
+    }
+
+    if (elem.classList.contains('move--down-10')) {
+      console.log(parseInt(entireCaption.style.top));
+      if (parseInt(entireCaption.style.top) > 75) return;
+      top += 10;
+      entireCaption.style.top = `${top}%`;
+    }
+
+    if (elem.classList.contains('font--bold')) {
+    }
+
+    if (elem.classList.contains('font--regular')) {
+    }
+
+    if (elem.classList.contains('font--italic')) {
+    }
+
+    if (elem.classList.contains('font--thin')) {
+    }
+
+    if (elem.classList.contains('finish')) {
+    }
+  });
+}
+
+startCombinating();
