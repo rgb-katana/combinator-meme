aa23421f03a93da477d11cdc32a20fbd628c4dbf
rgb-katana <komarova1610@yandex.ru>
2022-11-16 17:32:01 +0300

    added bold and italics also changed 2 buttons to left and right probably need to add some animations

--- script.js
+++ script.js
@@ -21,19 +21,77 @@ function startCombinating() {
   // Selecting phase.
   const handler = document.querySelector('.caption--handler');
   const entireCaption = document.querySelector('.caption');
+  const combinator = document.querySelector('.combinator');
   let top = 80;
   let left = 50;
+
+  // functions
+  function out(element) {
+    element.addEventListener('mouseout', function () {
+      element.style.backgroundColor = 'black';
+    });
+  }
+
+  // Handlers
+  handler.addEventListener('mouseover', function (e) {
+    const elem = e.target;
+    if (elem.classList.contains('move--up-1')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('move--down-1')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('move--up-10')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('move--down-10')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('font--bold')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('font--italic')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('move--left-1')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('move--right-1')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+
+    if (elem.classList.contains('finish')) {
+      elem.style.backgroundColor = 'green';
+      out(elem);
+    }
+  });
+
   handler.addEventListener('click', function (e) {
     const elem = e.target;
     console.log(elem);
     if (elem.classList.contains('move--up-1')) {
-      if (entireCaption.style.top === '10%') return;
+      if (parseInt(entireCaption.style.top) <= 10) return;
       top--;
       entireCaption.style.top = `${top}%`;
     }
 
     if (elem.classList.contains('move--down-1')) {
-      if (entireCaption.style.top === '85%') return;
+      if (parseInt(entireCaption.style.top) >= 85) return;
       top++;
       entireCaption.style.top = `${top}%`;
     }
@@ -53,18 +111,30 @@ function startCombinating() {
     }
 
     if (elem.classList.contains('font--bold')) {
-    }
-
-    if (elem.classList.contains('font--regular')) {
+      combinator.classList.toggle('boold');
     }
 
     if (elem.classList.contains('font--italic')) {
+      combinator.classList.toggle('itaalic');
     }
 
-    if (elem.classList.contains('font--thin')) {
+    if (elem.classList.contains('move--left-1')) {
+      if (left === 41) return;
+      left--;
+      entireCaption.style.left = `${left}%`;
+    }
+
+    if (elem.classList.contains('move--right-1')) {
+      console.log(left);
+      if (left === 59) return;
+      left++;
+      entireCaption.style.left = `${left}%`;
     }
 
     if (elem.classList.contains('finish')) {
+      document.querySelector('.caption--handler').style.opacity = '0';
+      entireCaption.style.background =
+        'linear-gradient(180deg, rgba(2,0,36,0) 40%, rgba(0,0,0,1) 40%, rgba(0,0,0,1) 100%)';
     }
   });
 }
